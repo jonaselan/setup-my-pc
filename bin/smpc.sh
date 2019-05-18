f62b4174fdc86cc58aa05d5d771049c2739a8423
jonaselan <jonas.elan@gmail.com>
2019-05-18 13:03:29 -0300

    Define scripts structure

--- bin/smpc.sh
+++ bin/smpc.sh
@@ -1,1 +1,30 @@
-echo 'sim';
+#!/bin/bash
+
+# Command line arguments
+_argc=$# # count of arguments
+_script=$0 # name of this script
+_block=$1 # block of commands to run
+_option=$2 # option related to the block
+_tag=$3 # tag to modify some behavior of the execution
+
+# importing functions
+. $SMPCPATH/bin/helpers.sh
+. $SMPCPATH/bin/dev.sh
+. $SMPCPATH/bin/personal.sh
+
+main() {
+  echo $SMPCPATH
+  echo '----'
+  echo $_argc
+  echo '----'
+  echo $_script
+  echo '----'
+  echo $_block
+  echo '----'
+  echo $_option
+  echo '----'
+  echo $_tag
+}
+
+#execute
+main
