f4a45622a952d956b2cfb24be9e983b3a89c1780
Jonas Elan <jonas.elan@gmail.com>
2020-10-30 18:27:48 -0300

    update

--- arch.sh
+++ arch.sh
@@ -6,23 +6,27 @@ sudo pacman-mirrors -ic Brazil
 
 # basic
 sudo pacman -Syu git vim wget curl copyq unzip xclip ncdu sxhkd flameshot zsh
+#ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# sxhkd: add autostart script -> /usr/bin/sxhkd
+# fusmua: add autostart script -> /.conf/fusuma/fusuma.sh
 
 # yay
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd .. && rm -fr yay
 
-# fun 
+# fun
 sudo yay -S fortune-mod cowsay
 
 # dev
 sudo yay -Syu nodejs npm albert xdotool ripgrep diff-so-fancy terminator postgresql alacritty tmux
 
 # personal
-sudo yay -Syu  tldr exa bat prettyping libsecret gnome-keyring aur/jumpapp-git
+sudo yay -Syu autojump tldr exa bat prettyping libsecret gnome-keyring aur/jumpapp-git
 
 # system
 sudo yay -Syu pulseaudio pavucontrol # audio control
 
-# shop: autojump, postman, vscode, chrome, dbeaver, mailspring
+# store: postman, vscode, chrome, dbeaver, mailspring
 
 # scripts
 cd /usr/local/bin && sudo wget -rnd https://dystroy.org/broot/download/x86_64-linux/broot && sudo chmod +x broot && cd -
@@ -36,6 +40,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 git clone https://github.com/wfxr/forgit.git "$ZSH_CUSTOM/plugins/forgit"
+git clone https://github.com/Aloxaf/fzf-tab "$ZSH_CUSTOM/plugins/fzf-tab"
 
 git clone https://github.com/jonaselan/dotfiles.git ~/.smpc
 # Setup folders
@@ -44,13 +49,16 @@ mkdir ~/.config/sxhkd/
 mkdir ~/.config/fusuma/
 
 # src:dest
+# "$HOME/.smpc/terminator:$HOME/.config/terminator/config"
 link_files=(
     "$HOME/.smpc/.zshrc:$HOME/.zshrc"
     "$HOME/.smpc/.vimrc:$HOME/.vimrc"
+    "$HOME/.smpc/.tmux:$HOME/.tmux"
     "$HOME/.smpc/.gitconfig:$HOME/.gitconfig"
-    "$HOME/.smpc/terminator:$HOME/.config/terminator/config"
     "$HOME/.smpc/sxhkdrc:$HOME/.config/sxhkd/sxhkdrc"
     "$HOME/.smpc/fusuma.yml:$HOME/.config/fusuma/config.yml"
+    "$HOME/.smpc/startship.toml:$HOME/.config/startship.toml"
+    "$HOME/.smpc/alacritty.yml:$HOME/.config/alacritty/alacritty.yml"
 )
 
 # Link files
